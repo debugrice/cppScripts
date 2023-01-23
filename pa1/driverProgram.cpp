027b0e91bf67d8a6d932d16eada8b0f8bef9d762
Christopher Rice <82599998+debugrice@users.noreply.github.com>
2023-01-23 16:19:56 -0600

    initial comit

---
+++ pa1/driverProgram.cpp
@@ -0,0 +1,37 @@
+#include "player.cpp"
+
+int main()
+{
+    playerClass newPlayer;
+    string FN;
+    string LN;
+    int matches;
+    int runs;
+    int balls;
+    int notouts;
+    string testSelector = "no";
+
+    cout << "Welcome to the Cricket Player Stats Program." << endl;
+    cout << "When prompted, please enter the player's data in the form of" << endl;
+    cout << "FirstName LastName Matches Runs Balls NotOuts" << endl;
+
+    while (testSelector == "no")
+    {
+        cout << "Enter Player Data: ";
+        cin >> FN >> LN >> matches >> runs >> balls >> notouts;
+
+        newPlayer.setFirstName(FN);
+        newPlayer.setLastName(LN);
+        newPlayer.setMatches(matches);
+        newPlayer.setRuns(runs);
+        newPlayer.setBalls(balls);
+        newPlayer.setNotOuts(notouts);
+
+        cout << newPlayer.getFirstName() + newPlayer.getLastName() + " Dissmissals: " , newPlayer.calcDismissals() , " Batting Strike Rate: " , newPlayer.calcBattingStrikeRate(), " Batting Average: " , newPlayer.calcBattingStrikeRate();
+        cout << "Do you wish to test another [yes/no]? ";
+        cin >> testSelector;
+    }
+
+
+    return 0;
+}
---
+++ pa1/player.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+using namespace std;
+
+class playerClass {
+public:
+
+    playerClass();
+    
+    string getFirstName();
+    string getLastName();
+    int getMatches();
+    int getRuns();
+    int getBalls();
+    int getNotOuts();
+
+    string setFirstName(string newFirstName);
+    string setLastName(string newLastName);
+    void setMatches(int newMatches);
+    void setRuns(int newRuns);
+    void setBalls(int newBalls);
+    void setNotOuts(int newNotOuts);
+
+    int calcDismissals();
+    int calcBattingAverage();
+    int calcBattingStrikeRate();
+
+    
+private:
+    string FirstName;
+    string LastName;
+    int Matches;
+    int Runs;
+    int Balls;
+    int NotOuts;
+    int Playerarr[4] = {Matches, Runs, Balls, NotOuts};
+};
+
+playerClass::playerClass()
+{
+    FirstName = "uknown";
+    LastName = "uknown";
+    Matches = 0;
+    Runs = 0;
+    Balls = 0;
+    NotOuts = 0;
+
+
+}
+
+string playerClass::getFirstName()
+{
+    return FirstName;
+}
+
+string playerClass::getLastName()
+{
+    return LastName;
+}
+
+
+int playerClass::getMatches()
+{
+    return Matches;
+}
+
+int playerClass::getRuns()
+{
+    return Runs;
+}
+
+int playerClass::getBalls()
+{
+    return Balls;
+}
+
+int playerClass::getNotOuts()
+{
+    return NotOuts;
+}
+
+string playerClass::setFirstName(string newFirstName)
+{
+    FirstName = newFirstName;
+}
+
+string playerClass::setLastName(string newLastName)
+{
+        LastName = newLastName;
+
+}
+
+void playerClass::setMatches(int newMatches)
+{
+    Matches = newMatches;
+}
+
+void playerClass::setRuns(int newRuns)
+{
+    Runs = newRuns;
+}
+
+void playerClass::setBalls(int newBalls)
+{
+    Balls = newBalls;
+}
+
+void playerClass::setNotOuts(int newNotOuts)
+{
+    NotOuts = newNotOuts;
+}
+
+int playerClass::calcDismissals()
+{
+    return (Matches - NotOuts);
+}
+
+int playerClass::calcBattingAverage()
+{
+    return (Runs/calcDismissals());
+}
+
+int playerClass::calcBattingStrikeRate()
+{
+    return (Runs/Balls)*100;
+}
+
+
