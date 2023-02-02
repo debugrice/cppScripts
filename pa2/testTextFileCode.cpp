a08865f0c72ecbff5b60042911e5c83f93f447f7
Christopher Rice <82599998+debugrice@users.noreply.github.com>
2023-02-01 23:31:38 -0600

    Testing switch double for loop

--- pa2/testTextFileCode.cpp
+++ pa2/testTextFileCode.cpp
@@ -26,22 +26,25 @@ int main()
     // Assign each component to a variable, for-loop through each component until a count of 6 is reached then move to the next array in the array of array
     for (int i = 0; i < 6; i++)
     {
-        for (int j = 0; j < 7; j++) {       
-        getline(inFile, txtFilearr[i][j], delim);
+        for (int j = 0; j < 7; j++) 
+        {       
+        
+            getline(inFile, txtFilearr[i][j], delim);
+            cout << "i" << i << endl;
+            cout << "j" << j << endl;
+            cout << "text file array: " << txtFilearr[i][j] << endl;
+
+            switch (j != 10 ) {
+            case 0: p[i].setFirstName(txtFilearr[i][j]);    break;
+            case 1: p[i].setLastName(txtFilearr[i][j]);     break;
+            case 2: p[i].setMatches(std::stoi(txtFilearr[i][j]));   break;
+            case 3: p[i].setBalls(std::stoi(txtFilearr[i][j]));     break;
+            case 4: p[i].setNotOuts(std::stoi(txtFilearr[i][j]));   break;
+            case 5: p[i].setRuns(std::stoi(txtFilearr[i][j]));  break;
+            default: break;
+            }
+
+            p[i].printPlayers();
         }
     }
-
-
-    // Step 2 Set each variable 
-    p[0].setFirstName(txtFilearr[0][0]);
-    p[0].setLastName(txtFilearr[0][1]);
-    p[0].setMatches(std::stoi(txtFilearr[0][2]));
-    p[0].setBalls(std::stoi(txtFilearr[0][3]));
-    p[0].setNotOuts(std::stoi(txtFilearr[0][4]));
-    p[0].setRuns(std::stoi(txtFilearr[0][5]));
-    p[0].printPlayers();
-
-
-    
-
 }
