b41507f2c2a99f43c28aaf6e6e349d4b9129aff5
Christopher Rice <82599998+debugrice@users.noreply.github.com>
2023-03-24 12:23:14 -0500

    Initial Commit

---
+++ EscapeGame/EscapeGameMode.h
@@ -0,0 +1,17 @@
+// CopyRight Christopher Rice 2022
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPEGAME_API AEscapeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
